2b01df649649e649e505f60ce4313a9dc982857a
Samuel <samuelenriquegonzalezperez2004@gmail.com>
2025-03-02 17:32:34 -0500

    initial commit

---
+++ src/calculo_area/Calculo_area.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package calculo_area;
+
+/**
+ *
+ * @author SAMUEL
+ */
+public class Calculo_area {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
